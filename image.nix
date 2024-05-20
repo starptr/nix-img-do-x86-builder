5cf34a6a233c4e5cfd96a0c6b5b99c77acf309a4
Yuto Nishida <yuto@berkeley.edu>
2024-05-20 01:45:06 -0700

    Fix config

--- image.nix
+++ image.nix
@@ -6,27 +6,29 @@
 let config = {
   imports = [ <nixpkgs/nixos/modules/virtualisation/digital-ocean-image.nix> ];
 
-  system.stateVersion = "23.11";
+  configuration = {
+    system.stateVersion = "23.11";
 
-  swapDevices = [
-    {
-      device = "/swapfile";
-      size = 3072;
-    }
-  ];
-
-  users.mutableUsers = false;
-  users.users.yuto = {
-    isNormalUser = true;
-    shell = pkgs.bash;
-    description = "Yuto";
-    password = "";
-    createHome = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-      # Add login public key or leave blank to fill via DO later
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtVvX9uhSWD1DPBIRqgkNzFXqjdqvWB/WtDy4seaiJl"
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = 3072;
+      }
     ];
+
+    users.mutableUsers = false;
+    users.users.yuto = {
+      isNormalUser = true;
+      shell = pkgs.bash;
+      description = "Yuto";
+      password = "";
+      createHome = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        # Add login public key or leave blank to fill via DO later
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtVvX9uhSWD1DPBIRqgkNzFXqjdqvWB/WtDy4seaiJl"
+      ];
+    };
   };
 };
 in
