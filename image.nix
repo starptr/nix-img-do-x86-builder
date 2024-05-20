ad7b148e60b50416cda5ece8086b178b43a073f1
Yuto Nishida <yuto@berkeley.edu>
2024-05-20 01:46:49 -0700

    Fix config

--- image.nix
+++ image.nix
@@ -16,19 +16,19 @@ let config = {
       }
     ];
 
-    users.mutableUsers = false;
-    users.users.yuto = {
-      isNormalUser = true;
-      shell = pkgs.bash;
-      description = "Yuto";
-      password = "";
-      createHome = true;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [
-        # Add login public key or leave blank to fill via DO later
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtVvX9uhSWD1DPBIRqgkNzFXqjdqvWB/WtDy4seaiJl"
-      ];
-    };
+    #users.mutableUsers = false;
+    #users.users.yuto = {
+    #  isNormalUser = true;
+    #  shell = pkgs.bash;
+    #  description = "Yuto";
+    #  password = "";
+    #  createHome = true;
+    #  extraGroups = [ "wheel" ];
+    #  openssh.authorizedKeys.keys = [
+    #    # Add login public key or leave blank to fill via DO later
+    #    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtVvX9uhSWD1DPBIRqgkNzFXqjdqvWB/WtDy4seaiJl"
+    #  ];
+    #};
   };
 };
 in
